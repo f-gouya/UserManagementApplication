ca2ab396e9e0f03170fdd7893d693720a0bb298e
f-gouya <farzad.gouya@live.com>
2024-07-17 12:04:14 +0330

    First Edition

--- PresentationLayer/profile.py
+++ PresentationLayer/profile.py
@@ -15,22 +15,22 @@ class ProfileFrame(Frame):
         self.header.grid(row=0, column=0, pady=10, padx=20, sticky="w")
 
         self.firstname_label = Label(self, text="First Name")
-        self.firstname_label.grid(row=1, column=0, pady=(0, 10), padx=20, sticky="w")
+        self.firstname_label.grid(row=1, column=0, pady=(0, 10), padx=(20, 0), sticky="w")
 
         self.current_user_firstname_label = Label(self)
-        self.current_user_firstname_label.grid(row=1, column=1, pady=(0, 10), padx=20, sticky="w")
+        self.current_user_firstname_label.grid(row=1, column=1, pady=(0, 10), padx=(0, 20), sticky="w")
 
         self.lastname_label = Label(self, text="Last Name")
-        self.lastname_label.grid(row=2, column=0, pady=(0, 10), padx=20, sticky="w")
+        self.lastname_label.grid(row=2, column=0, pady=(0, 10), padx=(20, 0), sticky="w")
 
         self.current_user_lastname_label = Label(self)
-        self.current_user_lastname_label.grid(row=2, column=1, pady=(0, 10), padx=20, sticky="w")
+        self.current_user_lastname_label.grid(row=2, column=1, pady=(0, 10), padx=(0, 20), sticky="w")
 
         self.username_label = Label(self, text="Username")
-        self.username_label.grid(row=3, column=0, pady=(0, 10), padx=20, sticky="w")
+        self.username_label.grid(row=3, column=0, pady=(0, 10), padx=(20, 0), sticky="w")
 
         self.current_user_username_label = Label(self, text="Username")
-        self.current_user_username_label.grid(row=3, column=1, pady=(0, 10), padx=20, sticky="w")
+        self.current_user_username_label.grid(row=3, column=1, pady=(0, 10), padx=(0, 20), sticky="w")
 
         self.change_password_button = Button(self, text="Change Password")
         self.change_password_button.grid(row=4, column=0, columnspan=2, pady=(0, 10), padx=20, sticky="ew")
