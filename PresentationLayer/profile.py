9dbe0e6962307788a863cad61a7f1e2c4045748f
f-gouya <farzad.gouya@live.com>
2024-07-22 08:02:57 +0330

    Second Edition

--- PresentationLayer/profile.py
+++ PresentationLayer/profile.py
@@ -7,28 +7,28 @@ class ProfileFrame(Frame):
 
         self.main_view = view
 
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
         self.header = Label(self, text="My Profile")
         self.header.grid(row=0, column=0, pady=10, padx=20, sticky="w")
 
-        self.firstname_label = Label(self, text="First Name")
+        self.firstname_label = Label(self, text="First Name:")
         self.firstname_label.grid(row=1, column=0, pady=(0, 10), padx=(20, 0), sticky="w")
 
         self.current_user_firstname_label = Label(self)
-        self.current_user_firstname_label.grid(row=1, column=1, pady=(0, 10), padx=(0, 20), sticky="w")
+        self.current_user_firstname_label.grid(row=1, column=1, pady=(0, 10), padx=20, sticky="w")
 
-        self.lastname_label = Label(self, text="Last Name")
+        self.lastname_label = Label(self, text="Last Name:")
         self.lastname_label.grid(row=2, column=0, pady=(0, 10), padx=(20, 0), sticky="w")
 
         self.current_user_lastname_label = Label(self)
-        self.current_user_lastname_label.grid(row=2, column=1, pady=(0, 10), padx=(0, 20), sticky="w")
+        self.current_user_lastname_label.grid(row=2, column=1, pady=(0, 10), padx=20, sticky="w")
 
-        self.username_label = Label(self, text="Username")
+        self.username_label = Label(self, text="Username:")
         self.username_label.grid(row=3, column=0, pady=(0, 10), padx=(20, 0), sticky="w")
 
         self.current_user_username_label = Label(self)
-        self.current_user_username_label.grid(row=3, column=1, pady=(0, 10), padx=(0, 20), sticky="w")
+        self.current_user_username_label.grid(row=3, column=1, pady=(0, 10), padx=20, sticky="w")
 
         self.back_button = Button(self, text="Back", command=self.show_home_frame)
         self.back_button.grid(row=4, column=0, columnspan=2, pady=10, padx=20, sticky="ew")
