3113ea9bdccd85901420f0ffefedd70d634058dc
f-gouya <farzad.gouya@live.com>
2024-07-22 10:33:16 +0330

    Second Edition

--- PresentationLayer/login.py
+++ PresentationLayer/login.py
@@ -20,12 +20,14 @@ class LoginFrame(Frame):
 
         self.username_entry = Entry(self)
         self.username_entry.grid(row=1, column=1, pady=(0, 10), padx=(0, 20), sticky="ew")
+        self.username_entry.bind("<Return>", lambda _: self.login())
 
         self.password_label = Label(self, text="Password")
         self.password_label.grid(row=2, column=0, pady=(0, 10), padx=10, sticky="w")
 
         self.password_entry = Entry(self, show="*")
         self.password_entry.grid(row=2, column=1, pady=(0, 10), padx=(0, 20), sticky="ew")
+        self.password_entry.bind("<Return>", lambda _: self.login())
 
         self.login_button = Button(self, text="Login", width=15, command=self.login)
         self.login_button.grid(row=3, column=1, pady=(0, 10), sticky="w")
